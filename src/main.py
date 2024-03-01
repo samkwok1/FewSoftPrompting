7c7b0bcf817a172ddf7bc3b273a1245ce8a0dd24
samkwok1 <samkwok1836@gmail.com>
2024-03-01 08:54:47 -0800

    add config

---
+++ src/main.py
@@ -0,0 +1,46 @@
+import hydra
+from omegaconf import DictConfig
+import os
+from train import train
+from evaluate import compute_stats
+from datasets import load_from_disk
+
+@hydra.main(config_path="config", config_name="config")
+def main(args: DictConfig) -> None:
+    sim = args.sim
+    env = args.env
+    ouput_save_path = f"{sim.repo_path}/outputs/{env.model_description.type}/{env.dataset}/{env.num_shots}/{sim.sim_id}"
+    model_save_path = f"models/{env.model_description.type}/{env.num_shots}"
+
+    print("Initializing dataset")
+    dataset = load_from_disk(f'datasets/FewSoftPrompting/{env.dataset}/{env.num_shots}shot')
+    train_dataset = dataset["train"]
+    eval_dataset = dataset["valid"] if env.dataset != "siqa" else dataset["test"]
+    test_dataset = dataset["test"] if env.dataset != "siqa" else dataset["valid"]
+
+    if env.train:
+        train(
+            model_path=env.model_description.model_path,
+            model_nickname=env.model_description.model_nickname,
+            tokenizer_path=env.model_description.tokenizer_path,
+            dataset=train_dataset,
+            training_params=env.training_params,
+            save_path=model_save_path
+        )
+    if env.fine_tune:
+        pass
+    if env.eval:
+        compute_stats(
+            model_path=env.model_description.model_path,
+            model_nickname=env.model_description.model_nickname,
+            tokenizer_path=env.model_description.tokenizer_path,
+            dataset=eval_dataset,
+            num_eval_shots=0,
+            save_path=model_save_path
+        )
+    if env.test:
+        pass
+
+if __name__ == '__main__':
+    main()
+    
---
+++ src/train.py
@@ -0,0 +1,56 @@
+from transformers import AutoModelForCausalLM, AutoTokenizer, TrainingArguments, Trainer, DataCollatorForLanguageModeling
+from peft import get_peft_config, get_peft_model, PromptTuningInit, PromptTuningConfig, TaskType, PeftType
+import torch
+
+def train(model_path, model_nickname, tokenizer_path, dataset, training_params, save_path):
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    assert device == "cuda"
+
+    print("Init model and tokenizer")
+    tokenizer = AutoTokenizer.from_pretrained(tokenizer_path, token="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")
+    tokenizer.pad_token = tokenizer.eos_token if tokenizer.pad_token is None else tokenizer.pad_token
+    tokenizer.pad_token_id = tokenizer.eos_token_id if tokenizer.pad_token_id is None else tokenizer.pad_token_id
+    #  model.resize_token_embeddings(len(tokenizer))
+
+    LLM_model = AutoModelForCausalLM.from_pretrained(
+        pretrained_model_name_or_path=model_path,
+        device_map='auto',
+        cache_dir = f"./{model_nickname}",
+        token="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"
+    )
+    print(LLM_model.config)
+
+    print("Init dataset")
+
+    dataset
+
+    num_virtual_tokens = training_params.num_virtual_tokens
+    print("Init PEFT")
+    peft_config = PromptTuningConfig(
+        task_type=TaskType.CAUSAL_LM,
+        prompt_tuning_init=PromptTuningInit.RANDOM,
+        num_virtual_tokens=num_virtual_tokens,
+        tokenizer_name_or_path=tokenizer_path
+    )
+    PEFT_model = get_peft_model(model=LLM_model, peft_config=peft_config)
+    PEFT_model.print_trainable_parameters()
+
+
+    print("Training")
+
+    learning_rate = training_params.learning_rate
+    num_epochs = training_params.num_epochs
+    training_args = TrainingArguments(
+        output_dir="outputs",
+        auto_find_batch_size=True,
+        learning_rate=learning_rate,
+        num_train_epochs=num_epochs
+    )
+    trainer = Trainer(
+        model=PEFT_model,
+        args=training_args,
+        train_dataset=dataset["train"],
+        data_collator=DataCollatorForLanguageModeling(tokenizer, mlm=False)
+    )
+    trainer.train()
+    trainer.model.save_pretrained(save_path)
